4f6aa3999ac6e594262f49d223ea5bf27f00d3dd
Ram kumar <webbieram@outlook.com>
2022-07-05 00:44:59 +0530

    initial commit

---
+++ app/app.js
@@ -0,0 +1,5 @@
+function startGame() {
+    var msg = document.getElementById('messages');
+    msg.innerHTML = 'Welcome to new game!';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
---
+++ app/app.ts
@@ -0,0 +1,6 @@
+function startGame(){
+    var msg = document.getElementById('messages')
+    msg.innerHTML = 'Welcome to new game!'
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
